d9e96cfb06f718a8d29e995bc80b95ec71b8ec8c
James Aikens <jaikens19@hotmail.com>
2021-02-12 13:01:58 -0600

    Start of phase 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import fruits from "./mockData/fruits.json";
+import FruitsIndex from './components/FruitsIndex'
 
 function App() {
   return (
     <>
       <h1>Welcome to Fruits App</h1>
+      <FruitsIndex fruits={fruits}/>
     </>
   );
 }
