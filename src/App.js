1079857f21606577f2c70f5d35e18fb6bd7ce49c
James Aikens <jaikens19@hotmail.com>
2021-02-12 19:48:36 -0600

    Completed phase 3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import FruitsIndex from './components/FruitsIndex'
 import fruits from './mockData/fruits.json'
 import FruitForm from './components/FruitForm'
 import FavoriteFruit from './components/FavoriteFruit'
+import SetFavoriteFruit from './components/SetFavoriteFruit'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <h1>Welcome to Fruits App</h1>
       <FruitsIndex fruits={fruits}/>
       <FruitForm fruits={fruits}/>
+      <SetFavoriteFruit fruits={fruits}/>
       <FavoriteFruit fruits={fruits}/>
     </>
   );
--- src/components/FavoriteFruit.js
+++ src/components/FavoriteFruit.js
@@ -4,14 +4,22 @@ import { useFavFruit } from '../context/FavFruitContext'
 const FavoriteFruit = ( {fruits} ) => {
   const { favFruitId } = useFavFruit()
   const favFruit = fruits.find(fruit => fruit.id === favFruitId)
-  console.log(fruits.name)
-
+  
+if(favFruit){
   return (
     <div className="fav-fruit">
       <h2>Favorite Fruit</h2>
-      <NavLink to={`/fruits/${favFruitId}`}>{favFruit.name}</NavLink>
+      <NavLink to={`/fruits/${favFruitId}`}>{favFruit ? favFruit.name : 'none'}</NavLink>
     </div>
   )
+
+} else {
+  return (
+  <>
+  </>
+
+  )
+}
 }
 
 export default FavoriteFruit;
